b247d1c57ff62e156b085ed73ebff1134bb33e5a
cx.li <chunxialims@163.com>
2021-01-03 10:44:31 +0800

    requestfacade responsefacade

---
+++ src/main/java/com/start/ex03/startup/Bootstrap.java
@@ -0,0 +1,11 @@
+package com.start.ex03.startup;
+
+
+import com.start.ex03.connector.http.HttpConnector;
+
+public final class Bootstrap {
+    public static void main(String[] args) {
+        HttpConnector connector = new HttpConnector();
+        connector.start();
+    }
+}
